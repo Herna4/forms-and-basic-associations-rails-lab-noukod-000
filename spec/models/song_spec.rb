9bc3185748f0e6ac4c21f0d1c424832908fc3fc2
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-09 14:36:27 +0000

    Automatically backed up by Learn

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Song, type: :model do
       expect(song.genre.name).to eq 'Rock'
     end
   end
-  
+
   describe 'genre_name' do
     it 'returns the genre name' do
       song = Song.new
@@ -16,7 +16,7 @@ RSpec.describe Song, type: :model do
       expect(song.genre_name).to eq 'Rock'
     end
   end
-  
+
   describe 'artist_name=' do
     it 'sets the artist to the artist of the specified name' do
       song = Song.new
@@ -24,7 +24,7 @@ RSpec.describe Song, type: :model do
       expect(song.artist.name).to eq 'Rock Hudson'
     end
   end
-  
+
   describe 'artist_name' do
     it 'returns the artist name' do
       song = Song.new
@@ -32,7 +32,7 @@ RSpec.describe Song, type: :model do
       expect(song.artist_name).to eq 'Rock Hudson'
     end
   end
-  
+
   describe 'note_contents' do
     it 'returns the content of all notes as an array' do
       song = Song.new
@@ -40,7 +40,7 @@ RSpec.describe Song, type: :model do
       expect(song.note_contents).to eq ['hi', 'there']
     end
   end
-  
+
   describe 'note_contents=' do
     it 'sets notes for a song' do
       song = Song.new
